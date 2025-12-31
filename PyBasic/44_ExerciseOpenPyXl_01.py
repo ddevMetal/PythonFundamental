fa894747bc616b5e35a2cbc0bd9a0abc0430894c
ddevMetal <145380370+ddevMetal@users.noreply.github.com>
2025-12-31 15:16:45 +0800

    added ex files

---
+++ PyBasic/44_ExerciseOpenPyXl_01.py
@@ -0,0 +1,11 @@
+from openpyxl import *
+import os
+
+# Example
+data = [
+    ['Room Number', 'Room Name' ,'Area (sqm)' ,'Occupancy', 'Finishes'         ],
+    ['101'        , 'Lobby'     , 35          , 50         , 'Marble Floor'    ],
+    ['102'        , 'Conference', 50          , 100        , 'Carpeted Floor'  ],
+    ['103'        , 'Office'    , 20          , 5          , 'Wooden Floor'    ]
+]
+
